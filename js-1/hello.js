ea3ec7455ae802121596a2eb3630b5412e541290
GFuentesG <g7fuentes7@gmail.com>
2023-05-12 19:08:12 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,6 @@
+function hello (name){
+    return "hola " + name + "!";
+}
+console.log(hello("Pedro")) // "Hola Pedro!"
+console.log(hello("Juan")) // "Hola Juan!"
+console.log(hello("")) // "Hola !"
