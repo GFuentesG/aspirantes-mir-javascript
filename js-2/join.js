0cdb1b21da12a0a66044e9a98e96992481da1ac9
GFuentesG <g7fuentes7@gmail.com>
2023-05-16 14:15:31 -0500

    solucion a los ejercicios de js

--- js-2/join.js
+++ js-2/join.js
@@ -2,13 +2,13 @@ function join(array){
     let stringTemplate = "";
     for (let i=0; i < array.length; i++){
 
-        stringTemplate = stringTemplate + array[i] + "" ;
-        console.log("Dentro del for", i, array[i], stringTemplate)
+        stringTemplate += array[i] + " " ;
+        //console.log("Dentro del for", i, array[i], stringTemplate)
     }
     return stringTemplate;
 }
 
 
-console.log(["Hola", "Mundo"])  // "Hola Mundo"
-console.log(["Make", "It", "Real"])  // "Make It Real"
-console.log([])  // ""
+console.log(join(["Hola", "Mundo"]))  // "Hola Mundo"
+console.log(join(["Make", "It", "Real"]))  // "Make It Real"
+console.log(join([]))  // ""
